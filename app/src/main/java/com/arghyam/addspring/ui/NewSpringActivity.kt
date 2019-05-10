eabda357a817cec8675ffdbd6569627de927cdd7
Stefy John <stefy@nineleaps.com>
2019-05-10 18:20:27 +0530

    Worked on New spring screen

---
+++ app/src/main/java/com/arghyam/addspring/ui/NewSpringActivity.kt
@@ -0,0 +1,13 @@
+package com.arghyam.addspring.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arghyam.R
+
+class NewSpringActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_spring)
+    }
+}
