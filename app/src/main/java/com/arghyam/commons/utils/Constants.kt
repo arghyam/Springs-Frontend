f6b9dfc2b1ffefaa57cf8b88d7ca754a5913abbf
Karthik <karthikreddy@nineleaps.com>
2019-05-13 12:07:45 +0530

    Added bug fix on back pressed

--- app/src/main/java/com/arghyam/commons/utils/Constants.kt
+++ app/src/main/java/com/arghyam/commons/utils/Constants.kt
@@ -3,4 +3,8 @@ package com.arghyam.commons.utils
 object Constants {
     var POOR_INTERNET_CONNECTION : String = "22"
     var PHONE_NUMBER : String = "phoneNumber"
+    var TAG_HOME: String = "home"
+    var TAG_SEARCH: String = "search"
+    var TAG_MY_ACTIVITY = "My Activtity"
+    var TAG_MORE = "More"
 }
--- app/src/main/java/com/arghyam/landing/ui/activity/LandingActivity.kt
+++ app/src/main/java/com/arghyam/landing/ui/activity/LandingActivity.kt
@@ -2,27 +2,29 @@ package com.arghyam.landing.ui.activity
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.FrameLayout
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.arghyam.R
+import com.arghyam.commons.utils.Constants.TAG_HOME
+import com.arghyam.commons.utils.Constants.TAG_MORE
+import com.arghyam.commons.utils.Constants.TAG_MY_ACTIVITY
+import com.arghyam.commons.utils.Constants.TAG_SEARCH
 import com.arghyam.landing.ui.fragment.HomeFragment
 import com.arghyam.more.ui.MoreFragment
 import com.arghyam.myactivity.ui.MyActivityFragment
 import com.arghyam.search.ui.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class LandingActivity : AppCompatActivity() {
 
-
+    var CURRENT_TAG : String = TAG_HOME
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
-        val fragment = HomeFragment.Companion.newInstance()
+        val fragment = HomeFragment.newInstance()
         addFragment(fragment)
     }
 
@@ -30,22 +32,26 @@ class LandingActivity : AppCompatActivity() {
         override fun onNavigationItemSelected(item: MenuItem): Boolean {
             when (item.itemId) {
                 R.id.navigation_home -> {
-                    val fragment = HomeFragment.Companion.newInstance()
+                    CURRENT_TAG = TAG_HOME
+                    val fragment = HomeFragment.newInstance()
                     addFragment(fragment)
                     return true
                 }
                 R.id.navigation_search -> {
-                    val fragment = SearchFragment.Companion.newInstance()
+                    CURRENT_TAG = TAG_SEARCH
+                    val fragment = SearchFragment.newInstance()
                     addFragment(fragment)
                     return true
                 }
                 R.id.navigation_my_activty -> {
-                    val fragment = MyActivityFragment.Companion.newInstance()
+                    CURRENT_TAG = TAG_MY_ACTIVITY
+                    val fragment = MyActivityFragment.newInstance()
                     addFragment(fragment)
                     return true
                 }
                 R.id.navigation_more -> {
-                    val fragment = MoreFragment.Companion.newInstance()
+                    CURRENT_TAG = TAG_MORE
+                    val fragment = MoreFragment.newInstance()
                     addFragment(fragment)
                     return true
                 }
@@ -65,6 +71,18 @@ class LandingActivity : AppCompatActivity() {
             .replace(R.id.content, fragment, fragment.javaClass.getSimpleName())
             .addToBackStack(fragment.javaClass.getSimpleName())
             .commit()
+    }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if(CURRENT_TAG == TAG_HOME) {
+            finish()
+        } else {
+            CURRENT_TAG = TAG_HOME
+            val fragment = HomeFragment.newInstance()
+            addFragment(fragment)
+        }
     }
 
 }
