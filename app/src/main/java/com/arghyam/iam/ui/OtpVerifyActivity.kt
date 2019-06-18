b473c09f76d326779538c2a7081030c2f96e5247
Anirudh <anirudh.mangalvedhe@nineleaps.com>
2019-06-18 11:10:01 +0530

    after 3 times otp resend show toast

--- app/src/main/java/com/arghyam/iam/ui/OtpVerifyActivity.kt
+++ app/src/main/java/com/arghyam/iam/ui/OtpVerifyActivity.kt
@@ -134,7 +134,7 @@ class OtpVerifyActivity : AppCompatActivity() {
 
     private fun initResendCodeButton() {
         resendCode.setOnClickListener {
-            if (resendOtpCount < 4) {
+            if (resendOtpCount <= 4) {
                 if (!isCounterRunning) {
                     makeResendOtpCall()
                 }
