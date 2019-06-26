987574d7d714c619e6981e649a2b0243fe0c1ff8
Stefy John <stefy@nineleaps.com>
2019-06-26 15:34:57 +0530

    Worked on search functionality

---
+++ app/src/main/java/com/arghyam/search/model/RequestRecentSearchDataModel.kt
@@ -0,0 +1,5 @@
+package com.arghyam.search.model
+
+import com.arghyam.additionalDetails.model.AdditionalDetailsModel
+
+class RequestRecentSearchDataModel (var recentSearchModel: RecentSearchModel)
---
+++ app/src/main/java/com/arghyam/search/viewmodel/SearchViewModel.kt
@@ -0,0 +1,51 @@
+package com.arghyam.search.viewmodel
+
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.arghyam.additionalDetails.repository.AdditionalDetailsRepository
+import com.arghyam.commons.di.ResponseListener
+import com.arghyam.commons.utils.SingleLiveEvent
+import com.arghyam.iam.model.RequestModel
+import com.arghyam.iam.model.ResponseModel
+import com.arghyam.search.repository.SearchRepository
+
+class SearchViewModel : ViewModel()  {
+
+    private var searchRepository: SearchRepository? = null
+    val mSearchData = MutableLiveData<ResponseModel>()
+    val mSearchDataError: SingleLiveEvent<String> = SingleLiveEvent()
+
+    fun setAdditionalDataRepository(searchRepository: SearchRepository) {
+        this.searchRepository = searchRepository
+    }
+
+    fun searchApi(mContext: Context, requestModel: RequestModel) {
+        searchRepository!!.searchDataApiRequest(
+            mContext,
+            requestModel,
+            object : ResponseListener<ResponseModel> {
+                override fun onSuccess(response: ResponseModel) {
+                    mSearchData.value = response
+                }
+
+                override fun onError(error: String?) {
+
+                    mSearchDataError.value = error
+                }
+
+                override fun onFailure(message: String?) {
+                    mSearchDataError.value = message
+                }
+            })
+    }
+
+    fun getSearchDataSuccess(): MutableLiveData<ResponseModel> {
+        return mSearchData
+    }
+
+    fun getSearchDataError(): SingleLiveEvent<String> {
+        return mSearchDataError
+    }
+}
